4b8bf612ac6061f07f3e0e87be489b0f567d9b0f
0rz1 <88487690+0rz1@users.noreply.github.com>
2021-08-07 19:24:14 +0800

    Add main func, read expression & return result

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"github.com/0rz1/calculator/calc"
 )
 
 func main() {
-	res, err := calc.Calc("1 + 2")
-	fmt.Println(res, err)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Calculator")
+	fmt.Println("---------------------")
+
+	for {
+		fmt.Print("-> ")
+		text, rerr := reader.ReadString('\n')
+		text = text[:len(text)-2]
+		if rerr != nil {
+			fmt.Printf("%v\n", rerr)
+		}
+		result, err := calc.Calc(text)
+		if err != nil {
+			fmt.Printf("%v err: %v\n", text, err)
+		} else {
+			fmt.Printf("%v = %v\n", text, result)
+		}
+	}
 }
